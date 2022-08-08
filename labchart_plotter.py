fcf4fa5aad4d62d4e18c2c5d19fb5ebfd9f3985a
jacob <jwgerlach00@gmail.com>
2022-08-07 20:14:08 -0400

    return header

--- labchart_plotter.py
+++ labchart_plotter.py
@@ -1,17 +1,24 @@
 import flask
 from flask import request
+from flask_cors import CORS
 
 # Sourced from pypi.com/jwgerlach
-import labchart_tools
+from labchart_tools import LCDataViewer
 
 
 app = flask.Flask(__name__)
+CORS(app, supports_credentials=True)
 
 
-@app.route('/plot', methods=['POST'])
-def plot():
+@app.route('/upload', methods=['POST'])
+def upload():
     if request.method == 'POST':
-        data = request.get_json()
-        text_data = data.text
-        
-        
+        file = request.files['file']
+        viewer = LCDataViewer(file)
+        return {
+            'header': viewer.trial_data[0].columns.values.tolist()
+        }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
